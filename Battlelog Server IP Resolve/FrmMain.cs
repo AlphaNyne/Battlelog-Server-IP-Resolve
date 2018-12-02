5faea7113534b5dd1a4e7e207e1fce384664cc1d
AlphaNyne <alphanyne@outlook.com>
2018-12-03 04:43:34 +0900

    プロジェクト ファイルを追加します。

---
+++ Battlelog Server IP Resolve/FrmMain.cs
@@ -0,0 +1,145 @@
+﻿/*
+ * Copyright (c) 2018 AlphaNyne
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EA.Battlelog.IPResolve
+{
+  public partial class FrmMain : Form
+  {
+    #region フィールド
+
+    private string _address;
+
+    private UIManager _uiManager;
+
+    #endregion
+
+    #region プロパティ
+
+    private UIManager UIManager {
+      get {
+        if (this._uiManager == null) {
+          this._uiManager = new UIManager();
+        }
+        return this._uiManager;
+      }
+      set {
+        this._uiManager = value;
+      }
+    }
+
+    #endregion
+
+    public FrmMain()
+    {
+      InitializeComponent();
+
+      this.UIManager.AddControl(new Control[] {
+        this.buttonOK,
+        this.textBoxURL,
+        this.linkLabelCopy
+      });
+    }
+
+    private async void ButtonOK_Click(object sender, EventArgs e)
+    {
+      WriteConsole("開始します...");
+      this.UIManager.Enabled = false;
+      this.textBoxResult.ResetText();
+
+      var result = await Task.Run(async () => {
+        var ipaddress = "";
+
+        try {
+          WriteConsole("ダウンロード中...");
+          var content = await GetResponseAsync(this.textBoxURL.Text);
+          WriteConsole("応答データの長さ: " + content.Length);
+          Thread.Sleep(500);
+
+          WriteConsole("応答データをオブジェクトに変換します");
+          var doc = new HtmlAgilityPack.HtmlDocument();
+          doc.LoadHtml(content);
+          Thread.Sleep(500);
+
+          WriteConsole("属性'data-ip'を検索しています...");
+          var node = doc.GetElementbyId("server-page");
+          Thread.Sleep(500);
+
+          try {
+            ipaddress = node.Attributes["data-ip"].Value;
+            WriteConsole("見つかりました");
+            WriteConsole("IP: " + ipaddress);
+          }
+          catch {
+            ipaddress = "";
+            WriteConsole("見つかりませんでした");
+          }
+        }
+        catch (Exception ex) {
+          MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        return ipaddress;
+      });
+
+      // リセット UI
+      this._address = result;
+      this.UIManager.Enabled = true;
+    }
+
+    private void WriteConsole(string content)
+    {
+      this.textBoxResult.WriteLine(content + Environment.NewLine);
+    }
+
+    private async Task<string> GetResponseAsync(string url)
+    {
+      if (string.IsNullOrEmpty(url)) {
+        throw new ArgumentNullException("The 'url' paramater is missing a value.");
+      }
+
+      var http = new HttpClient();
+      try {
+        var response = await http.GetAsync(url, HttpCompletionOption.ResponseContentRead);
+        if (response.IsSuccessStatusCode) {
+          return await response.Content.ReadAsStringAsync();
+        }
+      }
+      catch (Exception ex) {
+        MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+      }
+      finally {
+        http.Dispose();
+      }
+      return null;
+    }
+
+    private void LinkLabelCopy_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+      try {
+        Clipboard.SetText(this._address);
+      }
+      catch {
+        MessageBox.Show("クリップボードにコピーできませんでした。", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+      }
+    }
+  }
+}
---
+++ Battlelog Server IP Resolve/frmMain.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace EA.Battlelog.IPResolve {
+	partial class FrmMain {
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.labelHero = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.linkLabelCopy = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(16, 46);
+            this.textBoxURL.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(425, 23);
+            this.textBoxURL.TabIndex = 0;
+            this.textBoxURL.Text = "http://battlelog.battlefield.com/bf4/servers/show/pc/e9a4c7d6-85a9-49b3-b7bd-b1ba" +
+    "abe6b911/TB-1-NO-Explosive-AutoNuke-QQ181448680-YueLun-com/";
+            // 
+            // labelHero
+            // 
+            this.labelHero.AutoSize = true;
+            this.labelHero.Location = new System.Drawing.Point(14, 28);
+            this.labelHero.Name = "labelHero";
+            this.labelHero.Size = new System.Drawing.Size(160, 15);
+            this.labelHero.TabIndex = 1;
+            this.labelHero.Text = "バトルログ - サーバーページURL";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.BackColor = System.Drawing.Color.Black;
+            this.textBoxResult.ForeColor = System.Drawing.Color.Lime;
+            this.textBoxResult.Location = new System.Drawing.Point(16, 130);
+            this.textBoxResult.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResult.Size = new System.Drawing.Size(427, 186);
+            this.textBoxResult.TabIndex = 2;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(14, 111);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(44, 15);
+            this.labelResult.TabIndex = 3;
+            this.labelResult.Text = "結果 : ";
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(314, 78);
+            this.buttonOK.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(128, 26);
+            this.buttonOK.TabIndex = 4;
+            this.buttonOK.Text = "取得";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.ButtonOK_Click);
+            // 
+            // linkLabelCopy
+            // 
+            this.linkLabelCopy.AutoSize = true;
+            this.linkLabelCopy.Location = new System.Drawing.Point(405, 326);
+            this.linkLabelCopy.Name = "linkLabelCopy";
+            this.linkLabelCopy.Size = new System.Drawing.Size(34, 15);
+            this.linkLabelCopy.TabIndex = 6;
+            this.linkLabelCopy.TabStop = true;
+            this.linkLabelCopy.Text = "コピー";
+            this.linkLabelCopy.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabelCopy_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Do you hot to this address? xD";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 352);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabelCopy);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.labelHero);
+            this.Controls.Add(this.textBoxURL);
+            this.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Battlelog Server IP Resolve";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBoxURL;
+		private System.Windows.Forms.Label labelHero;
+		private System.Windows.Forms.TextBox textBoxResult;
+		private System.Windows.Forms.Label labelResult;
+		private System.Windows.Forms.Button buttonOK;
+		private System.Windows.Forms.LinkLabel linkLabelCopy;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
